ac3d343051d57aa4bb9ef24eaa9beea2df959e03
Manojkumar Saravanan <manoj@7semi.com>
2025-09-03 12:19:58 +0530

    Update Basic.ino

--- examples/Basic/Basic.ino
+++ examples/Basic/Basic.ino
@@ -1,3 +1,26 @@
+/*******************************************************
+ * @file Basic.ino
+ *
+ * @brief Basic example demonstrating usage of the 7Semi DS18B20 library.
+ *
+ * This example initializes a DS18B20 digital temperature sensor using
+ * the 1-wire protocol and reads the temperature in Celsius from the
+ * first discovered device on the bus.
+ *
+ * Key features demonstrated:
+ * - OneWire protocol setup
+ * - Device search and temperature readout
+ *
+ * @note This example requires the 7Semi DS18B20 library to be installed.
+ *
+ * @section author Author
+ * Written by 7Semi
+ *
+ * @section license License
+ * @license MIT
+ * Copyright (c) 2025 7Semi
+ *******************************************************/
+
 #include <7semi_DS18B20.h>
 
 DS18B20_7semi sensor(13);  // data pin 2
